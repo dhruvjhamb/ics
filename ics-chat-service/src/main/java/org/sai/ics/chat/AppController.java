257861dbeb82fa38a2fc1e4d0d2c938bf52310e1
Dhruv Jhamb <dhruvsaijhamb@gmail.com>
2019-01-02 11:56:29 -0800

    Finished basic web app setup (one step delay for chatbot response)

--- ics-chat-service/src/main/java/org/sai/ics/chat/AppController.java
+++ ics-chat-service/src/main/java/org/sai/ics/chat/AppController.java
@@ -1,13 +1,23 @@
 package org.sai.ics.chat;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AppController {
 
-    @RequestMapping("/hello")
-    public String hello() {
-        return "Hello world! - ics";
+    @PostMapping("/input")
+    public String botResponse(@RequestBody String input) {
+        System.out.println("Reached controller");
+        String botMessage = input;
+
+        if (input == "hi" || input =="hello") {
+            String[] hi = {"Hi", "Howdy", "Hello", "Greetings"};
+            botMessage = hi[(int) Math.floor((Math.random() * hi.length))];
+        }
+        if (input.contains("name")) {
+            botMessage = "My name is Chatty";
+        }
+
+        return botMessage;
     }
 }
---
+++ ics-webapp/src/main/webapp/script.js
@@ -0,0 +1,132 @@
+var messages = [], //array that hold the record of each string in chat
+lastUserMessage = "", //keeps track of the most recent input string from the user
+botMessage = "", //var keeps track of what the chatbot is going to say
+botName = 'Chatty'; //name of the chatbot
+/*talking = true; //when false the speech function doesn't work
+
+//text to Speech
+//https://developers.google.com/web/updates/2014/01/Web-apps-that-talk-Introduction-to-the-Speech-Synthesis-API
+function Speech(say) {
+  if ('speechSynthesis' in window && talking) {
+    var utterance = new SpeechSynthesisUtterance(say);
+    //msg.voice = voices[10]; // Note: some voices don't support altering params
+    //msg.voiceURI = 'native';
+    //utterance.volume = 1; // 0 to 1
+    //utterance.rate = 0.1; // 0.1 to 10
+    //utterance.pitch = 1; //0 to 2
+    //utterance.text = 'Hello World';
+    //utterance.lang = 'en-US';
+    speechSynthesis.speak(utterance);
+  }
+}*/
+
+//runs the keypress() function when a key is pressed
+document.onkeypress = keyPress;
+//if the key pressed is 'enter' runs the function newEntry()
+function keyPress(e) {
+  var x = e || window.event;
+  var key = (x.keyCode || x.which);
+  if (key == 13 || key == 3) {
+    //runs this function when enter is pressed
+    makeCorsRequest();
+  }
+}
+
+//clears the placeholder text ion the chatbox
+//this function is set to run when the users brings focus to the chatbox, by clicking on it
+function placeHolder() {
+  document.getElementById("chatbox").placeholder = "";
+}
+
+
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
+}
+
+// Create the XHR object.
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+}
+
+// Make the actual CORS request.
+function makeCorsRequest() {
+  // This is a sample server that supports CORS.
+  var url = 'http://localhost:8080/input';
+
+  var xhr = createCORSRequest('POST', url);
+  if (!xhr) {
+      alert('CORS not supported');
+      return;
+  }
+
+  // Response handlers.
+  xhr.onload = function() {
+      console.log("Got response");
+      botMessage = xhr.responseText;
+  };
+
+  xhr.onerror = function() {
+      alert('Woops, there was an error making the request.');
+  };
+
+  if (document.getElementById("chatbox").value != "") {
+      //pulls the value from the chatbox and sets it to lastUserMessage
+      lastUserMessage = document.getElementById("chatbox").value;
+   }
+
+  xhr.setRequestHeader("Content-type", "text/plain");
+  xhr.send(lastUserMessage);
+
+  display();
+}
+
+//It controls the overall input and output
+function display(xhr) {
+    //sets the chat box to be clear
+    document.getElementById("chatbox").value = "";
+    //adds the value of the chatbox to the array messages
+    messages.push(lastUserMessage);
+    //Speech(lastUserMessage);  //says what the user typed outloud
+    //sets the variable botMessage in response to lastUserMessage
+    //add the chatbot's name and message to the array messages
+    messages.push("<b>" + botName + ":</b> " + botMessage);
+    // says the message using the text to speech function written below
+    //Speech(botMessage);
+    //outputs the last few array elements of messages to html
+    for (var i = 1; i < 8; i++) {
+      if (messages[messages.length - i])
+        document.getElementById("chatlog" + i).innerHTML = messages[messages.length - i];
+    }
+}
